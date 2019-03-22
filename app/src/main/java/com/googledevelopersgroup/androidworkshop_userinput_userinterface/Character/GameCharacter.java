eddd8b610d687436126a4c4e63e565946f9880e0
Eddie <eherrerag.eh@gmail.com>
2019-03-21 19:26:14 -0600

    Converted the GameCharacter abstract class into an interface to let the children do their own implmentation of the update method.

--- app/src/main/java/com/googledevelopersgroup/androidworkshop_userinput_userinterface/Character/GameCharacter.java
+++ app/src/main/java/com/googledevelopersgroup/androidworkshop_userinput_userinterface/Character/GameCharacter.java
@@ -9,60 +9,27 @@ import com.googledevelopersgroup.androidworkshop_userinput_userinterface.Charact
 /**
  * Represents the super class that the characters of the game are going  to use in order to be inserted in the dame
  */
-abstract class GameCharacter {
-    private Bitmap bitmap; // the image of the character.
-    private int x, y; // the coordinates of the character
-    private int xVelocity, yVelocity;
-    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
-
-    {
-        xVelocity = 10;
-        yVelocity = 10;
-    }
-
-    public GameCharacter(Bitmap bitmap, int x, int y){
-        this.bitmap = bitmap;
-        this.x = x;
-        this.y = y;
-    }
-
-    public GameCharacter(Bitmap bitmap) {
-        this(bitmap,100,100);
-    }
+interface GameCharacter {
+    int SCREEN_WIDTH = Resources.getSystem().getDisplayMetrics().widthPixels;
+    int SCREEN_HEIGHT = Resources.getSystem().getDisplayMetrics().heightPixels;
 
     /**
      * going to draw the character in the canvas.
+     *
      * @param canvas - where the character is going to be drawn into.
      */
-    public void draw(Canvas canvas){
-        canvas.drawBitmap(bitmap, x, y, null);
-    }
+    void draw(Canvas canvas);
 
     /**
      * Going to handle the events of the character, for example, the movements of the character in the canvas.
+     *
      * @param movement - the movement of the character.
      */
-    public void update(CharacterMovement movement){
-        switch(movement){
-            case RIGHT:
-                if(x < screenWidth){//collision with the screen
-                    x += xVelocity;
-                }
-            case LEFT:
-                if(x > 0){//collision with the screen
-                    x -= xVelocity;
-                }
+    void update(CharacterMovement movement);
 
-        }
-    }
+    void setxVelocity(int xVelocity);
 
-    public void setxVelocity(int xVelocity) {
-        this.xVelocity = xVelocity;
-    }
+    void setyVelocity(int yVelocity);
 
-    public void setyVelocity(int yVelocity) {
-        this.yVelocity = yVelocity;
-    }
 }
 
--- app/src/main/java/com/googledevelopersgroup/androidworkshop_userinput_userinterface/Character/MainCharacter.java
+++ app/src/main/java/com/googledevelopersgroup/androidworkshop_userinput_userinterface/Character/MainCharacter.java
@@ -1,18 +1,51 @@
 package com.googledevelopersgroup.androidworkshop_userinput_userinterface.Character;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 import com.googledevelopersgroup.androidworkshop_userinput_userinterface.CharacterMovement;
 
-public class MainCharacter extends GameCharacter {
+public class MainCharacter implements GameCharacter {
+    private Bitmap bitmap;
+    private int x,y;
+    private int xVelocity, yVelocity;
 
     public MainCharacter(Bitmap bitmap, int x, int y) {
-        super(bitmap, x, y);
+        this.bitmap = bitmap;
+        this.x = x;
+        this.y = y;
     }
 
     public MainCharacter(Bitmap bitmap) {
-        super(bitmap);
+        this(bitmap, 100, 100);
+    }
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, x, y, null);
     }
 
+    @Override
+    public void update(CharacterMovement movement) {
+        switch(movement){
+            case RIGHT:
+                if(x < SCREEN_WIDTH){//collision with the screen
+                    x += xVelocity;
+                }
+            case LEFT:
+                if(x > 0){//collision with the screen
+                    x -= xVelocity;
+                }
 
+        }
+    }
+
+    @Override
+    public void setxVelocity(int xVelocity) {
+        this.xVelocity = xVelocity;
+    }
+
+    @Override
+    public void setyVelocity(int yVelocity) {
+        this.yVelocity = yVelocity;
+    }
 }
