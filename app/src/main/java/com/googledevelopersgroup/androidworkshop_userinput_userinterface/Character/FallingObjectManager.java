2b497afefdd5e3b7ffc6220ab39363e5a9e783bd
Eddie <eherrerag.eh@gmail.com>
2019-04-01 11:37:35 -0600

    Added the falling object managers and certain files to the gitignore

--- app/src/main/java/com/googledevelopersgroup/androidworkshop_userinput_userinterface/Character/FallingObjectManager.java
+++ app/src/main/java/com/googledevelopersgroup/androidworkshop_userinput_userinterface/Character/FallingObjectManager.java
@@ -1,5 +1,4 @@
 package com.googledevelopersgroup.androidworkshop_userinput_userinterface.Character;
 
 public class FallingObjectManager {
-    //TODO implement the methods for spawning and destroying asteroids
 }
